f939d12d55b9701ba3b501a7e38467b301c20bd1
tharrod1 <tharrod1@gmail.com>
2015-09-25 16:52:40 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,102 +1,137 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+			var scene = new THREE.Scene();
+			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+			var renderer = new THREE.WebGLRenderer();
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var MiningObj = new THREE.SphereGeometry(1, 6, 6);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var material1 = new THREE.MeshBasicMaterial({ color: 0x666666 });
-var material2 = new THREE.MeshBasicMaterial({ color: 0xd3d3d3 });
-var material3 = new THREE.MeshBasicMaterial({ color: 0x262626 });
-var cube = new THREE.Mesh(MiningObj, material);
+		//	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+		var geometry = new THREE.Geometry();
 
-var maplength = 30;
-var mapwidth = 40;
-scene.add(cube);
+			geometry.vertices.push(
+				new THREE.Vector3( -0.5, 0, -0.5 ),
+				new THREE.Vector3( 0.5, 0, -0.5 ),
+				new THREE.Vector3(  0.5, 0, 0.5 ),
 
-var hemiLight = new THREE.HemisphereLight(0xaabbff, 0x040404, 0.3);
+				new THREE.Vector3( -0.5, 0, -0.5 ),
+				new THREE.Vector3( -0.5, 0, 0.5 ),
+				new THREE.Vector3(  0.5, 0, 0.5 )
+			);
+			geometry.verticesNeedUpdate = true;
+			geometry.faces.push( new THREE.Face3( 0, 2, 1) );
+			geometry.faces.push( new THREE.Face3( 3, 4, 5) );
+			geometry.computeBoundingSphere();
+			var cylinder = new THREE.CylinderGeometry( 0.5, 0.6, 1, 32 );
+			var yellow = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+			var green = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+			var darkGreen = new THREE.MeshBasicMaterial( { color: 0x606f5b } );
+			var darkBrown = new THREE.MeshBasicMaterial( { color: 0x2e1d1d } );
+			var gray = new THREE.MeshBasicMaterial( { color: 0x666666 } );
+			var border = new THREE.MeshBasicMaterial( { color: 0x262626 } );
 
-// Treasure position
-cube.position.set(Math.floor((Math.random() * maplength) + 1) -1, 1, Math.floor((Math.random() * mapwidth) + 1)-1);
-if(cube.position.x == 0){
-    cube.position.x++;
-}
+			//position tester
+			//var mesh = new THREE.Mesh( geometry, yellow );
+			//mesh.position.set(2, 0 , 11);
+		//	scene.add( mesh );
+			var maplength = 50; //for across the screen
+			var mapwidth = 30; // for back and forth I know its kinda backwords but whatever for now
+			//Treasure generation
+			var Obstacle1 = new THREE.BoxGeometry( 1, 1, 1 );
+			var MiningObj = new THREE.SphereGeometry( 1, 6, 6 );
+			var cube = new THREE.Mesh( MiningObj, yellow );
+			cube.position.set(	Math.floor((Math.random() * maplength) + 1) -1,0.5,	Math.floor((Math.random() * mapwidth) + 1)-1);
+			if(cube.position.x == 0){
+				cube.position.x++;
+			}
+			if(cube.position.z == 0){
+				cube.position.z++;
+			}
+			if(cube.position.x == maplength - 1){
+				cube.position.x--;
+			}
+			if(cube.position.z == mapwidth - 1){
+				cube.position.z--;
+			}
+			scene.add(cube);
 
-if(cube.position.z == 0){
-    cube.position.z++;
-}
 
-if(cube.position.x == maplength - 1){
-    cube.position.x--;
-}
 
-if(cube.position.z == mapwidth - 1){
-    cube.position.z--;
-}
 
-//Obstacle generation
-for (var J = 0; J < 12; J++) {
-    var obstacle = new THREE.Mesh(geometry, material2);
-    obstacle.position.set(Math.floor((Math.random() * maplength) + 1) -1, 1, Math.floor((Math.random() * mapwidth) + 1)-1);
 
-    if (obstacle.position.z == cube.position.z && obstacle.position.x == cube.position.x) {
-	obstacle.position.z++;
-    }
-    
-    if(obstacle.position.x == 0) {
-	obstacle.position.x++;
-    }
-    
-    if(obstacle.position.z == 0) {
-	obstacle.position.z++;
-    }
-    
-    if(obstacle.position.x == maplength - 1) {
-	obstacle.position.x--;
-    }
-    
-    if(obstacle.position.z == mapwidth - 1) {
-	obstacle.position.z--;
-    }
-    
-    scene.add(obstacle);
-}
 
-// Map creation
-for (var x = 0; x < maplength; x++) {
-    for (z = 0; z < mapwidth; z++) {
-	var cube1 = new THREE.Mesh( geometry, material1 );
-	scene.add( cube1 );
-	cube1.position.set(x,0,z);
-    }
-}
+			//Obstacles generation
 
-// Map border creation
-for (var x = 0; x < maplength; x++) {
-    for (z = 0; z < mapwidth; z++) {
-	var cube1 = new THREE.Mesh( geometry, material3 );
-	if(x == 0 || z == 0 || x == maplength - 1 || z == mapwidth - 1){
-	    scene.add( cube1 );
-	    cube1.position.set(x,1,z);
-	}
-    }
-}
+			for (var J = 0; J < 12; J++) {
+								var obstacle = new THREE.Mesh( Obstacle1, gray );
+									obstacle.position.set(	Math.floor((Math.random() * maplength) + 1) -1,0.5,	Math.floor((Math.random() * mapwidth) + 1)-1);
+								if ( obstacle.position.z == cube.position.z &&  obstacle.position.x == cube.position.x ){
+									obstacle.position.z++;
+								}
+								if(obstacle.position.x == 0){
+									obstacle.position.x++;
+								}
+								if(obstacle.position.z == 0){
+									obstacle.position.z++;
+								}
+								if(obstacle.position.x == maplength - 1){
+									obstacle.position.x--;
+								}
+								if(obstacle.position.z == mapwidth - 1){
+									obstacle.position.z--;
+								}
+								scene.add( obstacle );
+						}
+			//MainMapTiles
+			for (var x = 0; x < maplength; x++) {
+							for (z = 0; z < mapwidth; z++) {
+									var cube1 = new THREE.Mesh( geometry, green );
+											cube1.position.set(x ,0,z);
+											scene.add( cube1 );
 
-camera.position.z = 40;
-camera.position.y = 20;
-camera.position.x = x/2;
-camera.rotation.x = -45;
+					}
+			}
+			for (var x = 0; x < maplength; x++) {
+							for (z = 0; z < mapwidth; z++) {
+									var cube1 = new THREE.Mesh( Obstacle1, border );
+									if(x == 0 || z == 0 || x == maplength - 1 || z == mapwidth - 1){
+											scene.add( cube1 );
+											cube1.position.set(x,0,z);
+										}
+					}
+			}
 
-var render = function () {
-    requestAnimationFrame(render);
+			var outsideThresh = -20;//needs to be negative
+			//Outside of map with tree generation
+			for (var x = outsideThresh; x < maplength - outsideThresh; x++) {
+							for (z = outsideThresh; z < mapwidth - outsideThresh; z++) {
+									var cube1 = new THREE.Mesh( geometry, darkGreen );
+									if(x <= 0 || z <= 0 || x >= maplength - 1 || z >= mapwidth - 1){
+											scene.add( cube1 );
+											cube1.position.set(x,0,z);
+											var sceneryObjectChance = Math.floor((Math.random() * 40)+1);
+											if (sceneryObjectChance == 9 && x <= -1 || sceneryObjectChance == 9 && z <= -1 || sceneryObjectChance == 9 && x >= maplength + 1 ||sceneryObjectChance == 9 && z >= mapwidth + 1){
+											var trunk = new THREE.Mesh( cylinder, darkBrown );
+											trunk.position.set(x,0.5,	z);
+											var treeTop = new THREE.Mesh( MiningObj, green );
+											treeTop.position.set(x,1.5,	z);
+											scene.add(trunk);
+											scene.add(treeTop);
+										}
+										}
+					}
+			}
 
-    //cube.rotation.x += 0.1;
-    //cube.rotation.y += 0.1;
+			camera.position.z = 40;
+			camera.position.y = 20;
+			camera.position.x = maplength/2;
+			camera.rotation.x = -45;
+			var render = function () {
+				requestAnimationFrame( render );
 
-    renderer.render(scene, camera);
-};
+			//	mesh.rotation.x += 0.1;
+				//mesh.rotation.y += 0.1;
 
-render();
+				renderer.render(scene, camera);
+			};
+
+			render();
